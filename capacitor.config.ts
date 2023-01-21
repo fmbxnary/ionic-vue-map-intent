328ec37e2b392e12d34452fd820a169cf6956b65
Fatih Mehmet Baydar <baydarmf@gmail.com>
2023-01-21 11:57:39 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-vue-map-intent',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
